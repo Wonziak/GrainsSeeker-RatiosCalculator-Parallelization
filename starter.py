250a3b00af5593dea83c4228e57720dd4920633b
Witold Dankiewicz <witold.dankiewicz98@gmail.com>
2022-05-02 14:35:53 +0200

    Parallelization of cv2.findContours.

--- starter.py
+++ starter.py
@@ -1,19 +1,26 @@
 from binaryImagesGenerator import generate_binary_images, show_layers
 from Config.ImageConfig import ImageConfig
-from countoursFinder import find_contours
-
+from countoursFinder import find_contours, find_contours_threading
 
 if __name__ == '__main__':
     image_path = 'RealImages/9600x9600.png'
+    # image_path = 'RealImages/fragmenty_kontury/DP800-200x200_kontury.png'
+    # color_map = ImageConfig.colors_map = {
+    #             'ferrite': (29, 143, 255),
+    #             'bainite': (172, 255, 46),
+    #             'martensite': (255, 0, 0)
+    # }
     image_config = ImageConfig.generate_image_info(image_path=image_path)
     phase_layers = generate_binary_images(method="GPU")
-    phase_layers = generate_binary_images(method="GPU")
-    phase_layers = generate_binary_images(method="CPU")
-    phase_layers = generate_binary_images(method="CPU")
-    phase_layers = generate_binary_images(method="SEQ")
-    phase_layers = generate_binary_images(method="SEQ")
+    # phase_layers = generate_binary_images(method="GPU")
+    # phase_layers = generate_binary_images(method="CPU")
+    # phase_layers = generate_binary_images(method="CPU")
+    # phase_layers = generate_binary_images(method="SEQ")
+    # phase_layers = generate_binary_images(method="SEQ")
 
-    # find_contours(phase_layers)
+    find_contours(phase_layers)
+    find_contours_threading(phase_layers)
+    find_contours_threading(phase_layers)
     # show_layers(phase_layers)
     # ratiosToCalculateList = ['Malinowska',
     #                          'Blair Bliss',
@@ -38,7 +45,6 @@ if __name__ == '__main__':
         'bainite': (0, 0, 255),
         'martensite': (255, 0, 0),
     }
-
 
     # x, y = Rc().calculate_ratios(image=image, background='bainite')
     # print(x, y)
