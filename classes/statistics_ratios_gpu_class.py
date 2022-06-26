31ddd43ecc39ff4dab442d4e94d7d4295c73acc2
WitoldDankiewicz <witold.dankiewicz98@gmail.com>
2022-06-26 16:49:25 +0200

    refactor.

--- classes/statistics_ratios_gpu_class.py
+++ classes/statistics_ratios_gpu_class.py
@@ -6,11 +6,16 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import defaultdict
+import time
 
 statsRatiosToCalculateList = ['BorderNeighbour',
                               'Dispersion',
                               'OnePointProbability',
                               'Linealpath']
+threadsperblock = 0
+blockspergrid_x = 0
+blockspergrid_y = 0
+blockspergrid = 0
 
 
 class StatisticsGPU:
@@ -23,57 +28,27 @@ class StatisticsGPU:
         self.calculatedRatios = {}
         self.grains = grains
         self.scale = scale
+        set_block()
 
     def blr(self):
+
+        start_time = time.time()
         colors = list(ic.colors_map.keys())
 
         for pair in itertools.combinations(colors, 2):
-            combination = pair[0]+"_" + pair[1]
+            combination = pair[0] + "_" + pair[1]
             self.borderNeighboursCountRatio[combination] = ic.color_number[pair[0]] + \
                                                            ic.color_number[pair[1]]
 
-        threadsperblock = (16, 16)
-        blockspergrid_x = math.ceil(ic.image.shape[0] / threadsperblock[0])
-        blockspergrid_y = math.ceil(ic.image.shape[1] / threadsperblock[1])
-        blockspergrid = (blockspergrid_x, blockspergrid_y)
         image_no_borders = ic.image
-        for i in range(14):
-            x_gpu = cuda.to_device(image_no_borders)
-            out_gpu = cuda.device_array_like(image_no_borders)
-            color_black_borders_as_color_on_left[blockspergrid, threadsperblock](x_gpu, out_gpu)
-            cuda.synchronize()
-            image_no_borders = out_gpu.copy_to_host()
+        image_no_borders = remove_borders(image_no_borders, 14)
 
-        # cv2.imshow('no borders', image_no_borders)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
+        numbers = map_pixels_to_colors(image_no_borders)
 
-        colors_as_numbers = np.zeros((ic.image.shape[0], ic.image.shape[1]))
-        x_gpu = cuda.to_device(image_no_borders)
-        out_gpu = cuda.device_array_like(colors_as_numbers)
-        for phase in ic.colors_map.keys():
-            if phase in ic.background:
-                continue
-            color = ic.colors_map[phase]
-            number = ic.color_number[phase]
-            iterate_on_image_and_assign_number[blockspergrid, threadsperblock] \
-                (x_gpu, out_gpu, color, number)
-            cuda.synchronize()
-        numbers = out_gpu.copy_to_host()
-
-        x_gpu = cuda.to_device(numbers)
-        out_gpu = cuda.device_array_like(numbers)
-        sum_neighbours_right[blockspergrid, threadsperblock](x_gpu, out_gpu)
-        cuda.synchronize()
-        summed_right = out_gpu.copy_to_host()
-
-        x_gpu = cuda.to_device(numbers)
-        out_gpu = cuda.device_array_like(numbers)
-        sum_neighbours_under[blockspergrid, threadsperblock](x_gpu, out_gpu)
-        cuda.synchronize()
-        summed_under = out_gpu.copy_to_host()
+        summed_right, summed_under = sum_neighbours(numbers)
         right_uniques, right_occurrences = np.unique(summed_right, return_counts=True)
         under_uniques, under_occurrences = np.unique(summed_under, return_counts=True)
+
         right_borders = {}
         under_borders = {}
 
@@ -93,10 +68,10 @@ class StatisticsGPU:
                 all_border_pixels += under_borders[value]
                 border_pixels[key] += under_borders[value]
 
-        self.borderNeighboursCountRatio = {k: v/all_border_pixels for k, v in border_pixels.items()}
-        print(self.borderNeighboursCountRatio)
-        print(all_border_pixels)
-        print(border_pixels)
+        self.borderNeighboursCountRatio = {k: v / all_border_pixels for k, v in
+                                           border_pixels.items()}
+
+        print("Border length ratio on GPU time is: " + str(time.time() - start_time))
 
 
 @cuda.jit
@@ -139,4 +114,62 @@ def sum_neighbours_under(layer_of_numbers, layer_of_numbers_sum_under):
     m, n = layer_of_numbers.shape[:2]
 
     if 0 <= i < n and 0 <= j < m:
-        layer_of_numbers_sum_under[j, i] = layer_of_numbers[j, i] + layer_of_numbers[j, i+1]
+        layer_of_numbers_sum_under[j, i] = layer_of_numbers[j, i] + layer_of_numbers[j, i + 1]
+
+
+def remove_borders(image_no_borders, iterations):
+    threadsperblock = (16, 16)
+    blockspergrid_x = math.ceil(ic.image.shape[0] / threadsperblock[0])
+    blockspergrid_y = math.ceil(ic.image.shape[1] / threadsperblock[1])
+    blockspergrid = (blockspergrid_x, blockspergrid_y)
+
+    for i in range(iterations):
+        x_gpu = cuda.to_device(image_no_borders)
+        out_gpu = cuda.device_array_like(image_no_borders)
+        color_black_borders_as_color_on_left[blockspergrid, threadsperblock](x_gpu, out_gpu)
+        cuda.synchronize()
+        image_no_borders = out_gpu.copy_to_host()
+    return image_no_borders
+
+
+def map_pixels_to_colors(image_no_borders):
+    colors_as_numbers = np.zeros((ic.image.shape[0], ic.image.shape[1]))
+    x_gpu = cuda.to_device(image_no_borders)
+    out_gpu = cuda.device_array_like(colors_as_numbers)
+    for phase in ic.colors_map.keys():
+        if phase in ic.background:
+            continue
+        color = ic.colors_map[phase]
+        number = ic.color_number[phase]
+        iterate_on_image_and_assign_number[blockspergrid, threadsperblock] \
+            (x_gpu, out_gpu, color, number)
+        cuda.synchronize()
+    numbers = out_gpu.copy_to_host()
+    return numbers
+
+
+def sum_neighbours(numbers):
+    x_gpu = cuda.to_device(numbers)
+    out_gpu = cuda.device_array_like(numbers)
+    sum_neighbours_right[blockspergrid, threadsperblock](x_gpu, out_gpu)
+    cuda.synchronize()
+    summed_right = out_gpu.copy_to_host()
+
+    x_gpu = cuda.to_device(numbers)
+    out_gpu = cuda.device_array_like(numbers)
+    sum_neighbours_under[blockspergrid, threadsperblock](x_gpu, out_gpu)
+    cuda.synchronize()
+    summed_under = out_gpu.copy_to_host()
+
+    return summed_right, summed_under
+
+
+def set_block():
+    global threadsperblock
+    global blockspergrid_x
+    global blockspergrid_y
+    global blockspergrid
+    threadsperblock = (16, 16)
+    blockspergrid_x = math.ceil(ic.image.shape[0] / threadsperblock[0])
+    blockspergrid_y = math.ceil(ic.image.shape[1] / threadsperblock[1])
+    blockspergrid = (blockspergrid_x, blockspergrid_y)
