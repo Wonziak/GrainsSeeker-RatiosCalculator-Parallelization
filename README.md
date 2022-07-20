1c5e8ce7db16cd2e8023ee512bf4238b8763d313
WitoldDankiewicz <witold.dankiewicz98@gmail.com>
2022-07-20 18:38:23 +0200

    typo fix, add mac starter

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ### Parallel Grains Seeker-Ratios Calculator in Python 3.9
 
 #### About project:
-This program calculates coefficients of grains of material microstructure. It uses CPU and GPU to parallelization.  
+This program calculates coefficients of grains of material microstructure. It uses CPU and GPU for parallelization.  
 GPU usage requires NVidia device and installed CUDA toolkit: https://developer.nvidia.com/cuda-toolkit.
 
 
@@ -14,6 +14,8 @@ GPU usage requires NVidia device and installed CUDA toolkit: https://developer.n
 #### To run no GPU version use command:
 'python starter_no_gpu.py'
 
-#### To save execution time run, command:
+#### To run on mac use command:
+'python starter_mac.py'
 
+#### To save execution time run command:
 'python starter.py > <file_name>.txt'
---
+++ starter_mac.py
@@ -0,0 +1,51 @@
+from binary_images_generator import generate_binary_images
+from config.image_config import ImageConfig
+from contours_finder import find_contours, find_contours_threading
+from statistics_classes.statistics_ratios_cpu_class import StatisticsCPU
+from statistics_classes.statistics_ratios_class import Statistics
+from grain_instances_generator import generate_grains_instances_threading, \
+    generate_grains_instances_sequentially, \
+    generate_grains_instances_sequentially_with_parallel_calculations_cpu, \
+    generate_grains_instances_threading_with_numba_cpu
+
+if __name__ == '__main__':
+    for image in ['200x200', '400x400', '800x800', '1600x1600', '3200x3200']:
+        print(f"\nCalculating for image {image}:\n")
+        image_path = f'RealImages/{image}.png'
+        image_config = ImageConfig.generate_image_info(image_path=image_path)
+        phase_layers = generate_binary_images(method="CPU")
+        phase_layers = generate_binary_images(method="CPU")
+        phase_layers = generate_binary_images(method="SEQ")
+
+        contours = find_contours(phase_layers)
+        find_contours_threading(phase_layers)
+
+        if image in ['200x200', '400x400']:
+            phase_grains_dict = generate_grains_instances_threading(contours)
+            phase_grains_dict = generate_grains_instances_sequentially(contours)
+
+        phase_grains_dict = generate_grains_instances_sequentially_with_parallel_calculations_cpu(contours)
+        phase_grains_dict = generate_grains_instances_sequentially_with_parallel_calculations_cpu(contours)
+
+        phase_grains_dict = generate_grains_instances_threading_with_numba_cpu(contours)
+        phase_grains_dict = generate_grains_instances_threading_with_numba_cpu(contours)
+
+        # phase_grains_dict = generate_grain_instances_threads_per_grain(contours)
+        # _ = create_series_from_ratios(phase_grains_dict)
+        # save_results_to_excel_file(phase_grains_dict, "seq_gpu.xlsx")
+
+        stats = Statistics(grains=phase_grains_dict, scale=1)
+        stats.lineal_path(20000)
+        stats.one_point_prob()
+        stats.dispersion()
+
+        statsCPU = StatisticsCPU(grains=phase_grains_dict)
+        statsCPU.lineal_path(20000)
+        statsCPU.lineal_path(20000)
+        statsCPU.one_point_prob()
+        statsCPU.one_point_prob()
+        statsCPU.blr()
+        statsCPU.blr()
+        statsCPU.dispersion()
+        print(f"\n Calculating for image {image} ended.\n")
+    exit(0)
